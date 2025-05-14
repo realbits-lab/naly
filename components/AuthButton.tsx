7c2abc501510d544bb0af46762a3495e51d58210
dev.realbits <dev.realbits@gmail.com>
2025-05-14 22:59:55 +0900

    refactor: Update Login and AuthButton components to improve structure and user state management

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,25 +1,26 @@
-import { createClient } from '@/utils/supabase/server'
+'use client'
+
 import Link from 'next/link'
-import { redirect } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { signOut } from '@/app/auth/signOut'
+import { createClient } from '@/utils/supabase/client'
+import type { User } from '@supabase/supabase-js'
 
-export default async function AuthButton() {
-  const supabase = await createClient()
+export default function AuthButton() {
+  const [user, setUser] = useState<User | null>(null)
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  const signOut = async () => {
-    'use server'
-
-    const supabase = await createClient()
-    await supabase.auth.signOut()
-    return redirect('/login')
-  }
+  useEffect(() => {
+    const fetchUser = async () => {
+      const supabase = createClient()
+      const { data: { user } } = await supabase.auth.getUser()
+      setUser(user)
+    }
+    fetchUser()
+  }, [])
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Hey, {user?.email}!
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
