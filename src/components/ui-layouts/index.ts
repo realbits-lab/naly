1918dd79193e8e5b3bf29dc0502849eb2d7bf495
dev.realbits <dev.realbits@gmail.com>
2025-06-10 13:00:34 +0900

    feat: Add background-shine animation to Tailwind configuration

---
+++ src/components/ui-layouts/index.ts
@@ -0,0 +1,16 @@
+// Utils and Hooks
+export { cn } from './lib/utils'
+export { useMediaQuery } from './hooks/use-media-query'
+export { useClickOutside } from './hooks/use-click-outside'
+export { useMounted } from './hooks/use-mounted'
+
+// Buttons
+export { default as ButtonBackgroundShine } from './buttons/btn-bg-shine'
+export { default as ButtonAnimatedGradient } from './buttons/btn-animated-gradient'
+export { default as ButtonHoverRight } from './buttons/btn-hover-right'
+
+// Core Components
+export { AnimatedBeam } from './animated-beam'
+export { default as SpotlightCard } from './spotlight-card'
+export { default as MouseTrail } from './mousetrail'
+export { default as Marquee } from './marquee' 
---
+++ src/components/ui-layouts/lib/utils.ts
@@ -0,0 +1,47 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+export function getRandomFromArray<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => func(...args), wait)
+  }
+}
+
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean
+  return (...args: Parameters<T>) => {
+    if (!inThrottle) {
+      func(...args)
+      inThrottle = true
+      setTimeout(() => (inThrottle = false), limit)
+    }
+  }
+} 
